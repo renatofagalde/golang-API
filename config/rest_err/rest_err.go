2d601765f46ddf215469a9c805d79c8fcc04ceb0
renatofagalde <renato@likwi.com.br>
2023-09-05 20:44:51 -0300

    estrutura de erros

---
+++ config/rest_err/rest_err.go
@@ -0,0 +1,44 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err     string  `json:"error"`
+	Code    int64   `json:"code"`
+	Causes  []Cause `json:"causes"`
+}
+
+type Cause struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+// construtor
+func NewRestErr(message, err string, code int64, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+// bad request
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+// badrequest validando objeto
+func NewBadRequestValidationError(message string, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
