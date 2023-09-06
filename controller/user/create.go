f526cf79d94865a02274ae84dff7851c3b6b2677
renatofagalde <renato@likwi.com.br>
2023-09-06 16:56:37 -0300

    client http para testes

--- controller/user/create.go
+++ controller/user/create.go
@@ -1,7 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"golang-basic/config/rest_err"
+	"golang-basic/controller/model/request"
+)
 
 func Create(c *gin.Context) {
-
+	var userRequest request.UserRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()),
+		)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
