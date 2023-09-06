da9bbc35cc75fa99cc0f7f1e80a6de121e88bf53
renatofagalde <renato@likwi.com.br>
2023-09-06 17:45:05 -0300

    ValidateUserError

--- config/validation/user-validate.go
+++ config/validation/user-validate.go
@@ -1,11 +1,14 @@
 package validation
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin/binding"
 	en "github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	en_translation "github.com/go-playground/validator/v10/translations/en"
+	"golang-basic/config/rest_err"
 )
 
 var (
@@ -20,4 +23,27 @@ func init() {
 		transl, _ = un.GetTranslator("en")
 		en_translation.RegisterDefaultTranslations(val, transl)
 	}
+}
+
+func ValidateUserError(validation_err error) *rest_err.RestErr {
+	var jsonErr *json.UnmarshalTypeError
+	var jsonValidationError validator.ValidationErrors
+
+	if errors.As(validation_err, jsonErr) {
+		return rest_err.NewBadRequestError("Invalid field type") //json enviando errado
+	} else if errors.As(validation_err, jsonValidationError) {
+		errorsCauses := []rest_err.Cause{}
+
+		for _, e := range validation_err.(validator.ValidationErrors) {
+			cause := rest_err.Cause{
+				Message: e.Translate(transl),
+				Field:   e.Field(),
+			}
+			errorsCauses = append(errorsCauses, cause)
+		}
+		return rest_err.NewBadRequestValidationError("Some fields are invalid", errorsCauses)
+	} else {
+		return rest_err.NewBadRequestError("Error trying to convert fields")
+	}
+
 }
