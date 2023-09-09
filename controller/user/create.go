fe3f855033ea34570d594ad75f75315d4cd66c08
renatofagalde <renato@likwi.com.br>
2023-09-09 13:32:17 -0300

    log com key journey

--- controller/user/create.go
+++ controller/user/create.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap/zapcore"
 	"golang-basic/config/logger"
 	"golang-basic/config/validation"
 	"golang-basic/controller/model/request"
@@ -11,12 +12,23 @@ import (
 func Create(c *gin.Context) {
 	var userRequest request.UserRequest
 
+	logger.Info("init create userController", zapcore.Field{
+		Key:    "journey",
+		String: "createUser",
+	})
+
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		errRest := validation.ValidateUserError(err)
-		logger.Error("Erro ao validar user", errRest)
+		logger.Error("Erro ao validar user", errRest, zapcore.Field{
+			Key:    "journey",
+			String: "createUser",
+		})
 		c.JSON(errRest.Code, errRest)
 		return
 	}
-	logger.Info("criando usuário %s")
+	logger.Info("init created userController", zapcore.Field{
+		Key:    "journey",
+		String: "createUser",
+	})
 	c.JSON(http.StatusOK, userRequest)
 }
