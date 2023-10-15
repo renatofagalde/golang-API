da42bd617979c272073bea6d135eb3b800efd04a
Renato Fagalde <renato@likwi.com.br>
2023-10-15 18:23:39 -0300

    mocking create user repository validate error

--- model/repository/create_user_repository_test.go
+++ model/repository/create_user_repository_test.go
@@ -30,17 +30,35 @@ func TestUserRepository_CreateUser(t *testing.T) {
 			{Key: "n", Value: 1},
 			{Key: "acknowledged", Value: true},
 		})
-
 		databasemock := mt.Client.Database(database_name)
-		repo := NewUserRepository(databasemock)
 
-		userDomain, err := repo.CreateUser(model.NewUserDomain(EMAIL, "password", "test", 18))
+		repo := NewUserRepository(databasemock)
+		domain := model.NewUserDomain(EMAIL, "password", "test", 18)
+		userDomain, err := repo.CreateUser(domain)
 
 		_, errID := primitive.ObjectIDFromHex(userDomain.GetID())
 
 		assert.Nil(t, err)
 		assert.Nil(t, errID) //validando o ID de forma indireta
-		assert.NotNil(t, userDomain.GetEmail(), EMAIL)
+		assert.NotNil(t, userDomain.GetEmail(), domain.GetEmail())
+		assert.NotNil(t, userDomain.GetName(), domain.GetName())
+		assert.NotNil(t, userDomain.GetPassword(), domain.GetPassword())
 
 	})
+
+	mtestDB.Run("when_sending_a_invalid_domain_return_error", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 0},
+		})
+		databasemock := mt.Client.Database(database_name)
+
+		repo := NewUserRepository(databasemock)
+		domain := model.NewUserDomain(EMAIL, "password", "test", 18)
+		userDomain, err := repo.CreateUser(domain)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, userDomain) //validando o ID de forma indireta
+
+	})
+
 }
