fb06f5ba435afd160cdf084230bac1e14b0fae47
Renato Fagalde <renato@likwi.com.br>
2023-11-04 10:07:28 -0300

    validando delete com id inválido e test

--- controller/user/delete_controller.go
+++ controller/user/delete_controller.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 	"golang-basic/config/logger"
+	"golang-basic/config/rest_err"
 	"net/http"
 )
 
@@ -11,6 +13,12 @@ func (uc *userControllerInterface) Delete(c *gin.Context) {
 	logger.Info("init delete userController")
 
 	id := c.Param("id")
+
+	if _, err := primitive.ObjectIDFromHex(id); err != nil {
+		errRest := rest_err.NewBadRequestError("Invalid ID")
+		c.JSON(errRest.Code, errRest)
+		return
+	}
 
 	err := uc.service.DeleteService(id)
 	if err != nil {
